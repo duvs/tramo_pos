589dd3a0a66b2a4f65e611a1905875d0e139fab7
Duvan Mendoza <duvanmendoza01@gmail.com>
2025-08-12 18:48:37 -0600

    feat: Enhance app localization and routing setup in MaterialApp

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:tramo_pos/core/config/router/app_router.dart';
 import 'package:tramo_pos/core/config/theme/app_theme.dart';
 import 'package:tramo_pos/features/product/presentation/list/product_list_page.dart';
 
@@ -7,6 +9,20 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Flutter Demo', theme: appTheme, home: ProductListPage());
+    return MaterialApp(
+      title: 'Tramo POS',
+      locale: const Locale('es', 'NI'),
+      supportedLocales: const [Locale('es', 'NI'), Locale('es')],
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      theme: appTheme,
+      navigatorKey: navigatorKey,
+      initialRoute: AppRoutes.products,
+      onGenerateRoute: AppRouter.onGenerateRoute,
+      home: ProductListPage(),
+    );
   }
 }
