be5a71146ddec4ee28b6d3e42c5e022177776515
Duvan Mendoza <duvanmendoza01@gmail.com>
2025-08-12 19:15:02 -0600

    fix: Change supabaseUrl and supabaseAnonKey to non-nullable types

--- lib/core/config/const/enviroment.dart
+++ lib/core/config/const/enviroment.dart
@@ -2,7 +2,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Enviroment {
   Enviroment._();
-  static String? supabaseUrl = dotenv.env['SUPABASE_URL'] ?? 'There is no supabase URL defined';
-  static String? supabaseAnonKey =
+  static String supabaseUrl = dotenv.env['SUPABASE_URL'] ?? 'There is no supabase URL defined';
+  static String supabaseAnonKey =
       dotenv.env['SUPABASE_ANON_KEY'] ?? 'There is no supabase anon key defined';
 }
