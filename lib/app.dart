3c539a659e9466a82f172a724476dfbbc4cb0387
Duvan Mendoza <duvanmendoza01@gmail.com>
2025-08-12 18:51:16 -0600

    feat: Remove home property from MaterialApp to streamline navigation

--- lib/app.dart
+++ lib/app.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:tramo_pos/core/config/router/app_router.dart';
 import 'package:tramo_pos/core/config/theme/app_theme.dart';
-import 'package:tramo_pos/features/product/presentation/list/product_list_page.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -22,7 +21,6 @@ class App extends StatelessWidget {
       navigatorKey: navigatorKey,
       initialRoute: AppRoutes.products,
       onGenerateRoute: AppRouter.onGenerateRoute,
-      home: ProductListPage(),
     );
   }
 }
