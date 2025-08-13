dd0fb5af81ec123d11ee50dae3559ef3ee99d4f9
Duvan Mendoza <duvanmendoza01@gmail.com>
2025-08-12 23:28:22 -0600

    feat: Wrap App widget with BlocProvider for AuthCubit integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tramo_pos/app.dart';
 import 'package:tramo_pos/di.dart';
+import 'package:tramo_pos/features/auth/presentation/authCubit/auth_cubit.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await initDependencies();
-  runApp(const App());
+  runApp(BlocProvider(create: (_) => getIt<AuthCubit>(), child: const App()));
 }
