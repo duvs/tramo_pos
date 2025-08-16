6183c82c66bf45234b70f7c2927026707d725422
Duvan Mendoza <duvanmendoza01@gmail.com>
2025-08-16 16:17:47 -0600

    refactor(profile): move `UserRepository` to `ProfileRepository`

---
+++ lib/features/profile/domain/repository/profile_repository.dart
@@ -0,0 +1,6 @@
+import 'package:tramo_pos/core/types/result.dart';
+import 'package:tramo_pos/features/profile/domain/entities/profile.dart';
+
+abstract class ProfileRepository {
+  Future<Result<Profile>> fetchMe();
+}
--- lib/features/user/domain/repository/user_repository.dart
+++
@@ -1,6 +0,0 @@
-import 'package:tramo_pos/core/types/result.dart';
-import 'package:tramo_pos/features/user/domain/entities/user.dart';
-
-abstract class UserRepository {
-  Future<Result<User>> fetchUser();
-}
