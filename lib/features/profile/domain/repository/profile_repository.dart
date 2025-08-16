9d42f3ce4d1f056f287af052cd764c9c24f5ccdb
Duvan Mendoza <duvanmendoza01@gmail.com>
2025-08-16 16:25:55 -0600

    refactor(profile): add user ID parameter to fetchMe method

--- lib/features/profile/domain/repository/profile_repository.dart
+++ lib/features/profile/domain/repository/profile_repository.dart
@@ -2,5 +2,5 @@ import 'package:tramo_pos/core/types/result.dart';
 import 'package:tramo_pos/features/profile/domain/entities/profile.dart';
 
 abstract class ProfileRepository {
-  Future<Result<Profile>> fetchMe();
+  Future<Result<Profile>> fetchMe(String userId);
 }
