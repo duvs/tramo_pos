1efdec0d867c532484572c37bb7a48aaf7d226c7
Duvan Mendoza <duvanmendoza01@gmail.com>
2025-08-17 20:52:53 -0600

    feat(ui): add home shell with product and profile pages

---
+++ lib/home_shell.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:tramo_pos/features/product/presentation/list/product_list_page.dart';
+import 'package:tramo_pos/features/profile/presentation/profile_page.dart';
+
+class HomeShell extends StatefulWidget {
+  const HomeShell({super.key});
+
+  @override
+  State<HomeShell> createState() => _HomeShellState();
+}
+
+class _HomeShellState extends State<HomeShell> {
+  int _index = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    final pages = const [ProductListPage(), ProfilePage()];
+    return Scaffold(
+      body: IndexedStack(index: _index, children: pages),
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: _index,
+        onDestinationSelected: (i) => setState(() => _index = i),
+        destinations: const [
+          NavigationDestination(
+            icon: Icon(Icons.inventory_2_outlined),
+            selectedIcon: Icon(Icons.inventory_2),
+            label: 'Productos',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.person_outline),
+            selectedIcon: Icon(Icons.person),
+            label: 'Perfil',
+          ),
+        ],
+      ),
+    );
+  }
+}
